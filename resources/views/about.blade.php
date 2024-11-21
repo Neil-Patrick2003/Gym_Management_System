1a01158c08d9fe127f689884f7003a0c8ca94545
barorok12 <159683181+barorok12@users.noreply.github.com>
2024-11-21 23:23:18 +0800

    fix-about-page

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -2,139 +2,143 @@
     <div class="contact">
         <div class="bg-gray-100" data-aos-easing="ease-in-out" data-aos-duration="1000">
             <!-- About Us Section -->
-            <section class="py-16 bg-white bg-opacity-80" style="padding-top: calc(100px + 2rem);" data-aos="fade-up">
+            <section class="py-16 bg-white bg-opacity-80" style="padding-top: calc(100px + 2rem); background-image: url('{{ asset('images/bg.png') }}'); background-size: cover; background-position: center;" data-aos="fade-up">
                 <!-- Adjusted padding-top to accommodate the fixed nav -->
-                <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+                <div class="w-full px-0">
                     <div class="text-center">
-                        <h2 class="text-base font-semibold text-red-600 tracking-wide uppercase">Our Journey</h2>
-                        <p class="mt-4 text-4xl leading-10 font-extrabold text-gray-900">Who We Are</p>
-                        <p class="mt-6 max-w-2xl text-xl text-gray-500 mx-auto">
-                            At FitnessHub, we are more than just a gym—we are a community. Our mission is to provide a
-                            space
-                            where everyone, regardless of fitness level, can push their limits and achieve their health
-                            goals. Located at Brgy. 7, Nasugbu, Batangas, we are proudly owned by Glenn Buenavista.
-                        </p>
+                        <h2 class="text-base font-black text-red-600 tracking-wide uppercase">Our Journey</h2>
+
+                        <div class="mt-4 bg-white border border-gray-300 rounded-lg p-8 max-w-4xl mx-auto">
+                        <p class="text-4xl leading-10 font-extrabold text-gray-900">Who We Are</p>
+                        <p class="mt-6 max-w-2xl text-xl text-gray-700 text-center mx-auto">
+                        At FitnessHub, we are more than just a gym—we are a community. Our mission is to provide a space where everyone, 
+                        regardless of fitness level, can push their limits and achieve their health goals. 
+                        Located at Brgy. 7, Nasugbu, Batangas, we are proudly owned by Glenn Buenavista.
+                    </p>
+
+</div>
+
+
+</div>
+
+
+
                     </div>
-                    <div class="mt-12">
-                        <dl
-                            class="space-y-12 md:space-y-0 md:grid md:grid-cols-2 lg:grid-cols-2 md:gap-x-8 md:gap-y-12">
-                            <div class="relative" data-aos="fade-up">
-                                <dt>
-                                    <div
-                                        class="absolute flex items-center justify-center h-12 w-12 rounded-md bg-red-600 text-white">
-                                        <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none"
-                                            viewBox="0 0 24 24" stroke="currentColor">
-                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                                d="M12 2C6.477 2 2 6.477 2 12s4.477 10 10 10 10-4.477 10-10S17.523 2 12 2zm0 10v8m4-4H8" />
-                                        </svg>
-                                    </div>
-                                    <p class="ml-16 text-lg leading-6 font-medium text-gray-900">State-of-the-Art
-                                        Equipment</p>
-                                </dt>
-                                <dd class="mt-2 ml-16 text-base text-gray-500">
-                                    We offer top-tier fitness equipment to help you get the most out of your workouts.
-                                    From cardio machines to free weights, our gym is fully equipped for all your fitness
-                                    needs.
-                                </dd>
-                            </div>
+                    <div class="mt-24 mx-16">
+    <dl class="space-y-12 md:space-y-0 md:grid md:grid-cols-2 lg:grid-cols-2 md:gap-x-8 md:gap-y-12">
+        
+        <!-- First Section: State-of-the-Art Equipment -->
+        <div class="relative bg-white rounded-xl p-8 shadow-lg" data-aos="fade-up">
+            <dt>
+                <div class="absolute flex items-center justify-center h-12 w-12 rounded-md bg-red-600 text-white">
+                    <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 2C6.477 2 2 6.477 2 12s4.477 10 10 10 10-4.477 10-10S17.523 2 12 2zm0 10v8m4-4H8" />
+                    </svg>
+                </div>
+                <p class="ml-16 text-lg leading-6 font-medium text-gray-900">State-of-the-Art Equipment</p>
+            </dt>
+            <dd class="mt-2 ml-16 text-base text-black">
+                We offer top-tier fitness equipment to help you get the most out of your workouts. 
+                From cardio machines to free weights, our gym is fully equipped for all your fitness needs.
+            </dd>
+        </div>
 
-                            <div class="relative" data-aos="fade-up">
-                                <dt>
-                                    <div
-                                        class="absolute flex items-center justify-center h-12 w-12 rounded-md bg-red-600 text-white">
-                                        <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none"
-                                            viewBox="0 0 24 24" stroke="currentColor">
-                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                                d="M3 10h18M3 14h12m6 0h-6" />
-                                        </svg>
-                                    </div>
-                                    <p class="ml-16 text-lg leading-6 font-medium text-gray-900">Personal Training</p>
-                                </dt>
-                                <dd class="mt-2 ml-16 text-base text-gray-500">
-                                    Our certified personal trainers are here to guide you every step of the way,
-                                    offering customized workout plans and motivation to help you reach your fitness
-                                    goals.
-                                </dd>
-                            </div>
+        <!-- Second Section: Personal Training -->
+        <div class="relative bg-white rounded-xl p-8 shadow-lg" data-aos="fade-up">
+            <dt>
+                <div class="absolute flex items-center justify-center h-12 w-12 rounded-md bg-red-600 text-white">
+                    <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 10h18M3 14h12m6 0h-6" />
+                    </svg>
+                </div>
+                <p class="ml-16 text-lg leading-6 font-medium text-gray-900">Personal Training</p>
+            </dt>
+            <dd class="mt-2 ml-16 text-base text-black">
+                Our certified personal trainers are here to guide you every step of the way,
+                offering customized workout plans and motivation to help you reach your fitness
+                goals.
+            </dd>
+        </div>
 
-                            <div class="relative col-span-1 md:col-span-1" data-aos="fade-up">
-                                <dt>
-                                    <div
-                                        class="absolute flex items-center justify-center h-12 w-12 rounded-md bg-red-600 text-white">
-                                        <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none"
-                                            viewBox="0 0 24 24" stroke="currentColor">
-                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                                d="M17 20h5v-2a5 5 0 00-5-5H7a5 5 0 00-5 5v2h5m5-5a3 3 0 100-6 3 3 0 000 6z" />
-                                        </svg>
-                                    </div>
-                                    <p class="ml-16 text-lg leading-6 font-medium text-gray-900">Nutritional Guidance
-                                    </p>
-                                </dt>
-                                <dd class="mt-2 ml-16 text-base text-gray-500">
-                                    Fitness isn't just about working out. Our nutrition experts will help you create a
-                                    balanced diet plan that complements your fitness routine for optimal results.
-                                </dd>
-                            </div>
+        <!-- Third Section: Nutritional Guidance -->
+        <div class="relative bg-white rounded-xl p-8 shadow-lg" data-aos="fade-up">
+            <dt>
+                <div class="absolute flex items-center justify-center h-12 w-12 rounded-md bg-red-600 text-white">
+                    <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 20h5v-2a5 5 0 00-5-5H7a5 5 0 00-5 5v2h5m5-5a3 3 0 100-6 3 3 0 000 6z" />
+                    </svg>
+                </div>
+                <p class="ml-16 text-lg leading-6 font-medium text-gray-900">Nutritional Guidance</p>
+            </dt>
+            <dd class="mt-2 ml-16 text-base text-black">
+                Fitness isn't just about working out. Our nutrition experts will help you create a
+                balanced diet plan that complements your fitness routine for optimal results.
+            </dd>
+        </div>
 
-                            <div class="relative col-span-1 md:col-span-1" data-aos="fade-up">
-                                <dt>
-                                    <div
-                                        class="absolute flex items-center justify-center h-12 w-12 rounded-md bg-red-600 text-white">
-                                        <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none"
-                                            viewBox="0 0 24 24" stroke="currentColor">
-                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                                d="M12 2a7 7 0 00-7 7c0 3.053 2.179 5.623 5.148 6.333a1 1 0 01.79.959v.396a1 1 0 01-1 1H6a1 1 0 01-1-1v-.396a1 1 0 01.79-.959C8.821 14.623 11 12.053 11 9a7 7 0 10-14 0 7 7 0 014.667 6.29" />
-                                        </svg>
-                                    </div>
-                                    <p class="ml-16 text-lg leading-6 font-medium text-gray-900">Expert Guidance</p>
-                                </dt>
-                                <dd class="mt-2 ml-16 text-base text-gray-500">
-                                    Our team comprises professionals from various fields, all working together to
-                                    deliver the best results for our clients.
-                                </dd>
-                            </div>
-                        </dl>
-                    </div>
+        <!-- Fourth Section: Expert Guidance -->
+        <div class="relative bg-white rounded-xl p-8 shadow-lg" data-aos="fade-up">
+            <dt>
+                <div class="absolute flex items-center justify-center h-12 w-12 rounded-md bg-red-600 text-white">
+                    <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 2a7 7 0 00-7 7c0 3.053 2.179 5.623 5.148 6.333a1 1 0 01.79.959v.396a1 1 0 01-1 1H6a1 1 0 01-1-1v-.396a1 1 0 01.79-.959C8.821 14.623 11 12.053 11 9a7 7 0 10-14 0 7 7 0 014.667 6.29" />
+                    </svg>
+                </div>
+                <p class="ml-16 text-lg leading-6 font-medium text-gray-900">Expert Guidance</p>
+            </dt>
+            <dd class="mt-2 ml-16 text-base text-black">
+                Our team comprises professionals from various fields, all working together to
+                deliver the best results for our clients.
+            </dd>
+        </div>
+
+    </dl>
+</div>
                 </div>
             </section>
 
             <!-- Gallery Section -->
             <section class="py-16 bg-white bg-opacity-80" data-aos="fade-up">
-                <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+                <div class="w-full px-0">
                     <h2 class="text-base font-semibold text-red-600 tracking-wide uppercase text-center">Gallery</h2>
                     <p class="mt-4 text-4xl leading-10 font-extrabold text-gray-900 text-center">Explore FitnessHub</p>
+                    <div class="px-8 mt-8">
                     <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 mt-8">
                         <div class="group overflow-hidden rounded-lg shadow-lg">
-                            <img src="gym1.jpg" alt="Gym Image 1"
+                            <img src="https://scontent.fmnl3-2.fna.fbcdn.net/v/t39.30808-6/295610700_163946462859439_1176053562882064592_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=833d8c&_nc_ohc=pzabMGdZKfkQ7kNvgFrookI&_nc_zt=23&_nc_ht=scontent.fmnl3-2.fna&_nc_gid=AdNYzp4EtOEuXeavz8LFRXF&oh=00_AYCSldEpz5CwEwNXNpGhYBLDqVO7u0PG2RGNL0mNm6lY9Q&oe=6745008D" alt="Gym Image 1"
                                 class="w-full h-48 object-cover transition-transform duration-300 transform group-hover:scale-105"
                                 data-aos="zoom-in">
                         </div>
                         <div class="group overflow-hidden rounded-lg shadow-lg">
-                            <img src="gym2.jpg" alt="Gym Image 2"
+                            <img src="https://scontent.fmnl37-1.fna.fbcdn.net/v/t39.30808-6/271857630_102714882321978_1308755724955575156_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=833d8c&_nc_ohc=pzk1PTwKa9EQ7kNvgHbfhHA&_nc_zt=23&_nc_ht=scontent.fmnl37-1.fna&_nc_gid=AWO3TJcZ-GobJzP49SQfTMt&oh=00_AYBLM3_-DzQE-oUNGO7xKQREPSPmi1lOWmLoSxosJWW3aw&oe=67450FE8" alt="Gym Image 2"
                                 class="w-full h-48 object-cover transition-transform duration-300 transform group-hover:scale-105"
                                 data-aos="zoom-in">
                         </div>
                         <div class="group overflow-hidden rounded-lg shadow-lg">
-                            <img src="gym3.jpg" alt="Gym Image 3"
+                            <img src="https://scontent.fmnl3-3.fna.fbcdn.net/v/t39.30808-6/271892258_102715182321948_7694001777508309385_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=833d8c&_nc_ohc=4hQzSQCAkxwQ7kNvgHPoItc&_nc_zt=23&_nc_ht=scontent.fmnl3-3.fna&_nc_gid=AYkIyKPYL0v80KvJBYHZuff&oh=00_AYBHqPvyrtCANm52PlInGMHA1CxhxggMSP2sZdxOHr0wvw&oe=6744F9FB" alt="Gym Image 3"
                                 class="w-full h-48 object-cover transition-transform duration-300 transform group-hover:scale-105"
                                 data-aos="zoom-in">
                         </div>
                         <div class="group overflow-hidden rounded-lg shadow-lg">
-                            <img src="gym4.jpg" alt="Gym Image 4"
+                            <img src="https://scontent.fmnl3-4.fna.fbcdn.net/v/t1.15752-9/462568459_1079514136778132_2559326647636589859_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=9f807c&_nc_ohc=L1FP4xZ4324Q7kNvgEF_ydU&_nc_zt=23&_nc_ht=scontent.fmnl3-4.fna&oh=03_Q7cD1QGUTXQhMPj1If3QLbQN6UC73t5skoaoL2mvXk1OihtCLA&oe=6766BD17" alt="Gym Image 4"
                                 class="w-full h-48 object-cover transition-transform duration-300 transform group-hover:scale-105"
                                 data-aos="zoom-in">
                         </div>
                         <div class="group overflow-hidden rounded-lg shadow-lg">
-                            <img src="gym5.jpg" alt="Gym Image 5"
+                            <img src="https://scontent.fmnl3-1.fna.fbcdn.net/v/t39.30808-6/271938972_102714905655309_2827216654182217282_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=833d8c&_nc_ohc=mfN_mfHzSikQ7kNvgHaFvpS&_nc_zt=23&_nc_ht=scontent.fmnl3-1.fna&_nc_gid=ArXh5Hg3wYBaPLDB4P2wilK&oh=00_AYDpw2o77_5enr7cjuQ1vCVm7zUKOzOLHSK_x885JK1xJg&oe=6744FC98" alt="Gym Image 5"
                                 class="w-full h-48 object-cover transition-transform duration-300 transform group-hover:scale-105"
                                 data-aos="zoom-in">
                         </div>
                         <div class="group overflow-hidden rounded-lg shadow-lg">
-                            <img src="gym6.jpg" alt="Gym Image 6"
+                            <img src="https://scontent.fmnl3-1.fna.fbcdn.net/v/t39.30808-6/272149561_102712005655599_8157976883478488774_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=833d8c&_nc_ohc=xI-Q9TOrflgQ7kNvgGXu6qV&_nc_zt=23&_nc_ht=scontent.fmnl3-1.fna&_nc_gid=AkprPm59lD1T_JvuPtNN427&oh=00_AYAUSed0KLo6XyQluNi7PjkWxAiW0d3LJTWZnOmW25nc1Q&oe=67452995" alt="Gym Image 6"
                                 class="w-full h-48 object-cover transition-transform duration-300 transform group-hover:scale-105"
                                 data-aos="zoom-in">
                         </div>
                     </div>
+                   
+
+                    </div>
+                  
                 </div>
             </section>
 
