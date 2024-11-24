4b4eea1e5c458f5bb9e936f0e531574051e07aef
Neil Patrick <neilpatrick.personal@gmail.com>
2024-11-24 23:25:37 +0800

    Auto stash before merge of "develop" and "origin/develop"

--- resources/views/trainer/tutorials/index.blade.php
+++ resources/views/trainer/tutorials/index.blade.php
@@ -5,16 +5,33 @@
         </div>
     </div>
 
-    @csrf
-    <button
-        class="open-modal-btn bg-transparent hover:bg-red-500 text-red-600 hover:text-white border border-red-500 font-bold py-2 px-4 rounded inline-flex items-center transition duration-300 ease-in-out"
-        data-target="myModal">
-        <svg class="w-[24px] h-[24px] text-red-500 hover:text-red mr-1.5" aria-hidden="true"
-            xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
-            <path fill-rule="evenodd"
-                d="M2 12C2 6.477 6.477 2 12 2s10 4.477 10 10-4.477 10-10 10S2 17.523 2 12Zm11-4.243a1 1 0 1 0-2 0V11H7.757a1 1 0 1 0 0 2H11v3.243a1 1 0 1 0 2 0V13h3.243a1 1 0 1 0 0-2H13V7.757Z"
-                clip-rule="evenodd" />
-        </svg>
+    {{-- add category && post tutorial --}}
+    <div class="flex justify-end gap-4 my-4">
+        {{-- category --}}
+        <form action="/trainer/tutorials/filter" method="POST" id="filterForm">
+            @csrf
+            <select id="categories" name="category_id"
+                class="px-4 py-2 bg-white text-red-500 border border-red-500 rounded shadow-sm focus:ring-red-500 focus:border-red-500 hover:">
+                <option value="All">All</option>
+                @foreach ($categories as $category)
+                    <option value="{{ $category->id }}">{{ $category->name }}</option>
+                @endforeach
+            </select>
+        </form>
+
+        <button
+            class="open-modal-btn bg-transparent hover:bg-red-500 text-red-600 hover:text-white border border-red-500 font-bold py-2 px-4 rounded inline-flex items-center transition duration-300 ease-in-out"
+            data-target="myModal">
+            <svg class="w-[24px] h-[24px] text-red-500 hover:text-red mr-1.5" aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor"
+                viewBox="0 0 24 24">
+                <path fill-rule="evenodd"
+                    d="M2 12C2 6.477 6.477 2 12 2s10 4.477 10 10-4.477 10-10 10S2 17.523 2 12Zm11-4.243a1 1 0 1 0-2 0V11H7.757a1 1 0 1 0 0 2H11v3.243a1 1 0 1 0 2 0V13h3.243a1 1 0 1 0 0-2H13V7.757Z"
+                    clip-rule="evenodd" />
+            </svg>
+
+            <span>Add Category</span>
+        </button>
 
         <span>Add Category</span>
     </button>
@@ -136,10 +153,6 @@
                                 class="flex-shrink-0 bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-white py-1 px-2 rounded"
                                 type="submit">
                                 Add
-                            </button>
-                            <button class="close-modal-btn" data-target="myModal"
-                                class="absolute top-2 right-2 text-gray-500 hover:text-gray-700">
-                                Cancel
                             </button>
                         </div>
                     </form>
